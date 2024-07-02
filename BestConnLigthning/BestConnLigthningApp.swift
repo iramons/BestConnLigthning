24fef835dcad444e386412eb7be9f8200f3d8091
Ramon <iramonsx@gmail.com>
2024-07-02 10:53:07 -0300

    chore(app): first commit - create project

---
+++ BestConnLigthning/BestConnLigthningApp.swift
@@ -0,0 +1,17 @@
+//
+//  BestConnLigthningApp.swift
+//  BestConnLigthning
+//
+//  Created by Ramon Santos on 02/07/24.
+//
+
+import SwiftUI
+
+@main
+struct BestConnLigthningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
